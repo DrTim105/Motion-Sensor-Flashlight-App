1064a367c5cd152c2f10eea3bf46d39e3ae73b7e
DrTim105 <Salihutimothy52@gmail.com>
2021-12-24 22:34:09 +0100

    updated code -added util class to deal with gesture control

--- app/src/main/java/com/salihutimothy/gestureflashlight/GestureDetectionService.kt
+++ app/src/main/java/com/salihutimothy/gestureflashlight/GestureDetectionService.kt
@@ -7,8 +7,11 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.hardware.camera2.CameraAccessException
 import android.os.IBinder
 import android.os.Vibrator
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class GestureDetectionService() : Service(), SensorEventListener {
 
@@ -17,16 +20,19 @@ class GestureDetectionService() : Service(), SensorEventListener {
     private lateinit var vibrator: Vibrator
     var lastShakeTime = 0L
     var isFlashLightOn = false
-//    val shakeThreshold
+    private val shakeThreshold = 10.0f
+    private lateinit var utility : Util
 
     override fun onCreate() {
         super.onCreate()
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        utility = Util (this)
 
-        if (sensorManager != null){
-//            accelerometer = sensorManager.getDefaultSensor()
-        }
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+
+
     }
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -34,7 +40,33 @@ class GestureDetectionService() : Service(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        TODO("Not yet implemented")
+        if (event != null) {
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                val currentTime = System.currentTimeMillis()
+
+                if ((currentTime - lastShakeTime) > MIN_TIME_BETWEEN_SHAKE){
+                    val x = event.values?.get(0)!!.toDouble()
+                    val y = event.values?.get(1)!!.toDouble()
+                    val z = event.values?.get(2)!!.toDouble()
+
+                    val acceleration = sqrt(x.pow(2.0)) +
+                            y.pow(2.0) + z.pow(2.0) - SensorManager.GRAVITY_EARTH
+
+                    if (acceleration > shakeThreshold) {
+                        lastShakeTime = currentTime
+                        if (!isFlashLightOn) {
+                            try {
+                                isFlashLightOn = utility.torchToggle("on")
+                            } catch (e : CameraAccessException) {
+                                e.printStackTrace()
+                            }
+                        } else {
+                            isFlashLightOn = utility.torchToggle("off")
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
---
+++ app/src/main/java/com/salihutimothy/gestureflashlight/Util.kt
@@ -0,0 +1,32 @@
+package com.salihutimothy.gestureflashlight
+
+import android.content.Context
+import android.hardware.camera2.CameraManager
+import android.os.Build
+
+class Util (private val context: Context) {
+
+    private var isSwitchedOn = false
+
+    fun torchToggle(command: String) : Boolean{
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+            val cameraId: String? = cameraManager.cameraIdList[0]
+
+            if (cameraId != null) {
+                isSwitchedOn = if (command == "on") {
+                    cameraManager.setTorchMode(cameraId, true)
+                    true
+                } else {
+                    cameraManager.setTorchMode(cameraId, false)
+                    false
+                }
+            }
+
+        }
+
+        return isSwitchedOn
+    }
+
+}
